bd917a5aab6401ebfed0fb69988b75e52215cb73
umutertakus <umutertakus@hotmail.com>
2023-04-13 09:38:20 +0300

    Update error handling

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,45 +9,53 @@ dotenv.config();
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { username, password, fullName } = req.body;
+  try {
+    const { username, password, fullName } = req.body;
 
-  const user = await UserModel.findOne({ username });
+    const user = await UserModel.findOne({ username });
 
-  if (user) {
-    return res.status(409).json({ message: "User already exists." });
+    if (user) {
+      return res.status(409).json({ message: "User already exists." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({
+      fullName,
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = new UserModel({
-    fullName,
-    username,
-    password: hashedPassword,
-  });
-  await newUser.save();
-
-  res.status(201).json({ message: "User registered successfully." });
 });
 
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const user = await UserModel.findOne({ username });
+    const user = await UserModel.findOne({ username });
 
-  if (!user) {
-    return res.status(404).json({ message: "User doesn't exists." });
+    if (!user) {
+      return res.status(404).json({ message: "User doesn't exists." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: "Username or password is incorrect." });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    res.status(200).json({ token, userId: user._id, fullName: user.fullName });
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) {
-    return res
-      .status(401)
-      .json({ message: "Username or password is incorrect." });
-  }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
-  res.status(200).json({ token, userId: user._id, fullName: user.fullName });
 });
 
 export { router as userRouter };
