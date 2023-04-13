0d36c7b44bfb2a8bde0db66dc21fe4d39659e26a
umutertakus <umutertakus@hotmail.com>
2023-04-13 20:52:26 +0300

    Add delete operation

--- server/src/routes/todos.js
+++ server/src/routes/todos.js
@@ -14,7 +14,7 @@ router.post("/addTodo", async (req, res) => {
     });
     await todo.save();
 
-    res.status(201).json({ message: "Todo added successfully." });
+    res.status(201).json(todo);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -25,8 +25,11 @@ router.get("/getTodos/:userId", async (req, res) => {
     const userId = req.params.userId;
 
     const todos = await TodoModel.find({
-      owner: { $in: userId },
+      owner: userId,
     });
+
+    if (todos.length === 0)
+      return res.status(200).json({ message: "You haven't added a task yet." });
 
     res.status(200).json(todos);
   } catch (err) {
@@ -43,7 +46,19 @@ router.put("/updateTodo", async (req, res) => {
     todo.completed = completed;
     await todo.save();
 
-    res.status(200).json({ message: "Todo updated successfully." });
+    res.status(200).json(todo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/deleteTodo/:todoId", async (req, res) => {
+  try {
+    const todoId = req.params.todoId;
+
+    await TodoModel.findByIdAndDelete(todoId);
+
+    res.status(200).json(todoId);
   } catch (err) {
     res.status(500).json(err);
   }
