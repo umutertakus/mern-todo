76a9e8421ad6a4a587f762024c01fc8a0ebc776c
umutertakus <umutertakus@hotmail.com>
2023-04-13 23:32:56 +0300

    Remove version keys

--- server/src/models/Todos.js
+++ server/src/models/Todos.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const TodoSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
+const TodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
   },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 export const TodoModel = mongoose.model("todos", TodoSchema);
--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 export const UserModel = mongoose.model("users", UserSchema);
